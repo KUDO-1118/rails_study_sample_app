4a80f36edfed5bc95fbe7b9d600e40e531571019
KUDO <au4saf010@gmail.com>
2024-07-14 05:52:36 +0000

    destroyのテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destory]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destory
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,8 +41,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def destory
-    User.find(params[:id]).destory
+  def destroy
+    User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url, status: :see_other
   end
@@ -70,7 +70,8 @@ class UsersController < ApplicationController
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 
-    def admin_user
+     # 管理者かどうか確認
+     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 end
